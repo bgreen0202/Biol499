b89a77bbd697aa9808ac47ce18d449a8381c30fc
bgreen0202 <bgreen@ualberta.ca>
2025-04-24 10:00:44 -0600

    Added JavaDocs

---
+++ slime/SLIMErToPython/models/lipidDictionary.py
@@ -0,0 +1,44 @@
+lipid_definitions = {
+    "PG 14:0-16:1": {
+        "backbone": "phosphatidylglycerol",
+        "backbone_id": "s_0089[c]",
+        "tails": ["C14:0", "C16:1"],
+        "abundance": {"BG8": 11.8}
+    },
+    "PG 16:1-16:1": {
+        "backbone": "phosphatidylglycerol",
+        "backbone_id": "s_0089[c]",
+        "tails": ["C16:1", "C16:1"],
+        "abundance": {"BG8": 17.1}
+    },
+    "PE 16:1-14:0": {
+        "backbone": "phosphatidylethanolamine",
+        "backbone_id": "s_0089[c]",
+        "tails": ["C16:1", "C14:0"],
+        "abundance": {"BG8": 1.2}
+    },
+    "PE 16:0-16:1": {
+        "backbone": "phosphatidylethanolamine",
+        "backbone_id": "s_0089[c]",
+        "tails": ["C16:0", "C16:1"],
+        "abundance": {"BG8": 12.4}
+    },
+    "PE 16:1-16:1": {
+        "backbone": "phosphatidylethanolamine",
+        "backbone_id": "s_0089[c]",
+        "tails": ["C16:1", "C16:1"],
+        "abundance": {"BG8": 19.0}
+    },
+    "PE 18:1-16:1": {
+        "backbone": "phosphatidylethanolamine",
+        "backbone_id": "s_0089[c]",
+        "tails": ["C18:1", "C16:1"],
+        "abundance": {"BG8": 12.5}
+    },
+    "PE 16:0-18:1": {
+        "backbone": "phosphatidylethanolamine",
+        "backbone_id": "s_0089[c]",
+        "tails": ["C16:0", "C18:1"],
+        "abundance": {"BG8": 7.0}
+    }
+}
---
+++ slime/SLIMErToPython/models/tailNames.py
@@ -0,0 +1,17 @@
+tail_aliases = [
+    ("C10:0", "Decanoate", "Decanoic acid"),
+    ("C12:0", "Laurate", "Lauric acid"),
+    ("C14:0", "Myristate", "Myristic acid"),
+    ("C15:0", "Pentadecanoate", "Pentadecanoic acid"),
+    ("C16:0", "Palmitate", "Palmitic acid"),
+    ("C16:1n9", "Palmitoleate (n-9 isomer)", "Palmitoleic acid (n-9 isomer)"),
+    ("C16:1n7", "Palmitoleate", "Palmitoleic acid"),
+    ("C16:1n6", "Hexadecenoate (n-6)", "Hexadecenoic acid (n-6)"),
+    ("C16:1n5", "Hexadecenoate (n-5)", "Hexadecenoic acid (n-5)"),
+    ("C16 unknown1", "Unknown C16 fatty acid", "none"),
+    ("C16:2", "Hexadecadienoate", "Hexadecadienoic acid"),
+    ("C16:3", "Hexadecatrienoate", "Hexadecatrienoic acid"),
+    ("C18", "Stearate", "Stearic acid"),
+    ("C18:1n9", "Oleate", "Oleic acid"),
+    ("C18:1n7", "Vaccenat", "Vaccenic acid")
+]
